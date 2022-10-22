66a5d786601767fa64010047ed4810f72b4860a4
Nicolás de Ory <deorynicolas@gmail.com>
2022-10-23 01:17:40 +0200

    fixed compilation error

--- frontend/pages/api/createAccount.ts
+++ frontend/pages/api/createAccount.ts
@@ -1,22 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 /**
  * Paso contraseña "Metamask" por parametro
  * A partir de esta contraseña, genero semilla y
  * la encripto con esa contraseña.
- * 
+ *
  * Devuelvo la address
- * @param req 
- * @param res 
+ * @param req
+ * @param res
  */
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // Llamado cuando se hace 
+  // en el cliente axios.get('/api/createAccount')
+
+  const body = req.body;
+  // { id: nft}
+  res.status(200).json({ name: "John Doe" });
 }

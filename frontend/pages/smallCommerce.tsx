e3ac412ef92f92a5bb57ea7cf1b86b7065d645fd
Ignacio Navarro <inavarroblazquez@gmail.com>
2022-10-23 11:20:48 +0200

    Now files are uploaded to pinata

--- frontend/pages/smallCommerce.tsx
+++ frontend/pages/smallCommerce.tsx
@@ -1,71 +1,56 @@
 import type { NextPage } from "next";
 import TopBar from "../components/TopBar";
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { create, CID, IPFSHTTPClient } from "ipfs-http-client";
+import { sendFileToIPFS, ipfsJSON } from "../components/ipfs";
 
 
-
-
-
-const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const files = (form[6] as HTMLInputElement).files;
-
-
-
-    if (!files || files.length === 0) {
-        return alert("No files selected");
-    }
-
-    const file = files[0];
-    console.log("Uploading file: ", file.name, file);
-
-
-    // upload files
-    /*
-    const result = await (ipfs as IPFSHTTPClient).add(file);
-
-    const uniquePaths = new Set([
-        ...images.map((image) => image.path),
-        result.path,
-    ]);
-    const uniqueImages = [...uniquePaths.values()]
-        .map((path) => {
-            return [
-                ...images,
-                {
-                    cid: result.cid,
-                    path: result.path,
-                },
-            ].find((image) => image.path === path);
-        });
-
-    // @ts-ignore
-    setImages(uniqueImages);
-
-    form.reset();
-    */
-};
-
-
-
-
-
-//const [nombreEmpresa, setNombreEmpresa] = React.useState([]);
-//const [nombreColeccion, setNombreColeccion] = React.useState([]);
-//const [ventajaHolder, setventajaHolder] = React.useState([]);
-//const [ventajaUso, setventajaUso] = React.useState([]);
 
 
 
 
 const SmallCom: NextPage = () => {
+    const [buffer, setBuffer] = useState("");
+    //const [nombreEmpresa, setNombreEmpresa] = React.useState([]);
+    //const [nombreColeccion, setNombreColeccion] = React.useState([]);
+    //const [ventajaHolder, setventajaHolder] = React.useState([]);
+    //const [ventajaUso, setventajaUso] = React.useState([]);
+
+
+    const onChangeInput = async (event: React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        const fileInput = event.target as HTMLInputElement;
+        const files = fileInput.files;
+
+        if (!files || files.length === 0) {
+            return alert("No files selected");
+        }
+
+        const file = files[0];
+        console.log("Uploading file: ", file.name, file);
+
+        sendFileToIPFS(file);
+
+
+
+        /*
+        const result = await (ipfs).add(file);
+        
+        
+        console.log("Uploaded file:", result.path, result.cid.toString());
+        
+        */
+    };
+
+
+
+
+
     return (
         <div>
             <TopBar title="Tokenización Aqua" />
             <div className="bg-white pt-6">
-                <form className="flex flex-col items-center justify-center w-full h-full" onSubmit={onSubmitHandler}>
+                <form className="flex flex-col items-center justify-center w-full h-full" onSubmit={() => { }}>
                     <div className="pb-6">
                         <p className="select-none w-full transition-colors relative drop-shadow-md py-2 text-gray-600 font-extrabold flex items-center justify-start">Nombre de la empresa:</p>
                         <input type={"text"} className="w-80 h-10 bg-gray-100 border-b-[1px] border-gray-600 p-2 outline-none" placeholder={"Nombre de la empresa"} required />
@@ -95,8 +80,7 @@ const SmallCom: NextPage = () => {
                     </div>
                     <div className="pb-[30px]">
                         <input type='file' name="file" className="" onChange={(e: React.FormEvent<HTMLInputElement>) => {
-                            const newValue = e.currentTarget.value;
-                            console.log(newValue);
+                            onChangeInput(e);
                         }} />
                     </div>
 
